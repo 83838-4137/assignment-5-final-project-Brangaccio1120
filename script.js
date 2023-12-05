87b244bf44365db52f7d430d8c1479bc35eb077a
Brangaccio1120 <143016101+Brangaccio1120@users.noreply.github.com>
2023-12-05 14:50:05 -0500

    changing button
    changing the button so that it isn't wrapped in an a tag

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const hamburgerMenu = document.querySelector('.hamburgerMenu');
 hamburgerMenu.addEventListener('click', () => {nav.classList.toggle('nav--toggle');
 hamburgerMenu.classList.toggle('hamburgerMenu--toggle');
 });
+
+
+const resultsList = document.getElementById('forum-post')
+new URLSearchParams(window.location.search).forEach((value,name) => {
+  resultsList.append(`${name}: ${value}`)
+  resultsList.append(document.createElement('br'))
+})
